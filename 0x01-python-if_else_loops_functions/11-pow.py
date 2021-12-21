687e7eaf0a508c9cb3c0a02a72cd4c1be43076cd
Alexoat76 <3915@holbertonschool.com>
2021-12-21 16:16:19 -0500

    a to the power of b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 11-pow.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Return a to the power of b."""
+
+
+def pow(a, b):
+    return (a ** b)
