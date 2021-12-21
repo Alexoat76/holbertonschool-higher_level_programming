70e0bba62578309204062eb199bb6a9efa810825
Alexoat76 <3915@holbertonschool.com>
2021-12-21 12:16:41 -0500

    print alphabet ASCII lowercase

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File: 2-print_alphabet.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+        print("{}".format(chr(letter)), end="")
