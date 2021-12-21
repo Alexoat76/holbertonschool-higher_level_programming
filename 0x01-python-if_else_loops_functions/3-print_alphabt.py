f42e0c6fde9df1541235b90ac8c7ac8c4bad8f52
Alexoat76 <3915@holbertonschool.com>
2021-12-21 13:02:55 -0500

    print lowercase excpt q e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,7 +3,7 @@
 # Author: Alex Orland Arévalo Tribaldos
 # email: <3915@holbertonschool.com>
 
-"""Print the alphabet in lowercase, not followed by a new line, and except 'q' and 'e'"""
+"""Print the alphabet in lowercase, except 'q' and 'e'"""
 for letter in range(97, 123):
-            if chr(letter) != 'q' and chr(letter) != 'e':
-                            print("{}".format(chr(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
