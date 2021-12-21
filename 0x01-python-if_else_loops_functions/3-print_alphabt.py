18cea29353c9bb9013951e8790be1144a1b06e2e
Alexoat76 <3915@holbertonschool.com>
2021-12-21 12:18:03 -0500

    print alphabet ASCII except e-q

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 3-print_alphabet.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Print the alphabet in lowercase, not followed by a new line, and except 'q' and 'e'"""
+for letter in range(97, 123):
+            if chr(letter) != 'q' and chr(letter) != 'e':
+                            print("{}".format(chr(letter)), end="")
