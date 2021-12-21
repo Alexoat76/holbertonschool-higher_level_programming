d210d6d21f698726451e8ec178480310b78ba71f
Alexoat76 <3915@holbertonschool.com>
2021-12-21 16:04:12 -0500

    adds two integers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 9-print_last_digit.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Return the addition of a and b."""
+
+
+def add(a, b):
+    return (a + b)
