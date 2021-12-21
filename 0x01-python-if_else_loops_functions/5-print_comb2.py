d1c88736780759782fe2d17b8a0e4d837f813287
Alexoat76 <3915@holbertonschool.com>
2021-12-21 13:18:41 -0500

    print nums 0-99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: 5-print_comb2.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Print all numbers from 0 to 99 separated by ',' followed by a space"""
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
