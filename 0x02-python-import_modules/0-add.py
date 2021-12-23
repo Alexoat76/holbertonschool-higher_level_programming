044cc8c530f153f9e7c67240b6e88e26f301d385
Alexoat76 <3915@holbertonschool.com>
2021-12-23 18:54:32 -0500

    Import function Add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: 0-add.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Print the sum of 1 and 2."""
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
