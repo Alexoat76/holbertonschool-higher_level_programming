11206992767eccb8509b30a4625c8ab5dcefb370
Alexoat76 <3915@holbertonschool.com>
2021-12-23 19:17:19 -0500

    imports variable from file

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 5-variable_load.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Print the value of variable a from variable_load_5."""
+
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print("{}".format(a))
