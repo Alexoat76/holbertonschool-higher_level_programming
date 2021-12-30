e9891df40ee0d3a9afc5d24f222ede542a08f0d5
Alexoat76 <3915@holbertonschool.com>
2021-12-30 10:58:29 -0500

    fix code again

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -2,8 +2,5 @@
 # File: 101-easy_print.py
 # Author: Alex Orland Arévalo Tribaldos
 # email: <3915@holbertonschool.com>
-
 """Prints '#pythoniscool', followed by a new line, in the standard output"""
-
-
 __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
