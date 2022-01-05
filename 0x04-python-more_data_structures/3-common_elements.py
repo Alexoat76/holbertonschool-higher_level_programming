778e8aaeed821ff940a3363963705609d634f5c6
Alexoat76 <3915@holbertonschool.com>
2022-01-05 10:31:05 -0500

    Ret set of common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 3-common_elements.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Return a set of common elements in two sets."""
+
+
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
