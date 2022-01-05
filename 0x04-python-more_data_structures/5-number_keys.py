f2e37f689438fca343194e9533aedd37ba5e6b4a
Alexoat76 <3915@holbertonschool.com>
2022-01-05 10:32:23 -0500

    Ret number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 5-number_keys.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Return the number of keys in a dictionary."""
+
+
+def number_keys(a_dictionary):
+    return (len(a_dictionary))
