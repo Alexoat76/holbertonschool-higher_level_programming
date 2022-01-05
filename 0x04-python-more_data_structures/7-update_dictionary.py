faf8558bba2ef4fb604d880200757e5d0941fd87
Alexoat76 <3915@holbertonschool.com>
2022-01-05 10:33:40 -0500

    Replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: 7-update_dictionary.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Replace or add key/value pairs in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)
