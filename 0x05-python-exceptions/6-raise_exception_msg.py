3fa229ab452a75d1254fef402aabaf222ce5e371
Alexoat76 <3915@holbertonschool.com>
2022-01-17 14:38:31 -0500

    Raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 6-raise_exception_msg.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Raise a NameError exception with a message."""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
