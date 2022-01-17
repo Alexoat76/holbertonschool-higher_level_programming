3c21d5109e895a62351e6cc646ed62f70cecd1c7
Alexoat76 <3915@holbertonschool.com>
2022-01-17 14:36:59 -0500

    Raises a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 5-raise_exception.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Raise a TypeError exception."""
+
+
+def raise_exception():
+    raise TypeError
