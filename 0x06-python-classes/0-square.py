58570f05bd9edf8ff0b7dc2184eb2fc47ca9db6c
Alexoat76 <3915@holbertonschool.com>
2022-01-18 16:11:48 -0500

    Defines empty class Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File: 0-square.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+This is the "Square"  module.
+This module provides a simple Square class.
+"""
+
+
+class Square:
+    """A simple Square class block"""
+    pass
