0143926a5d1a1fa504ae08e505bac0c5881fe983
Alexoat76 <3915@holbertonschool.com>
2022-01-18 17:15:11 -0500

    Defines a square by 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# File: 1-square.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+This is the "Square"  module.
+This module provides a simple Square class with initialize size.
+"""
+
+
+class Square:
+    """A class that defines a Square by size"""
+    def __init__(self, size):
+        self.__size = size
