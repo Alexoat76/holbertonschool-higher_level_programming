98a3cb2431b83fa816fec900dc2972863da55940
Alexoat76 <3915@holbertonschool.com>
2022-01-24 19:49:24 -0500

    Defines empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File: 0-rectangle.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+This is the "Rectangle"  module.
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+    pass
