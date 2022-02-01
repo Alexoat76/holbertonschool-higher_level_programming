db6eeac387dbfc5805a62738966fbdfa998d70ad
Alexoat76 <3915@holbertonschool.com>
2022-01-31 21:23:27 -0500

    Improve Geometry | Base on

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# File: 6-base_geometry.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+This module defines a base geometry class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
