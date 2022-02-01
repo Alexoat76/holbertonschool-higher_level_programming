6853a0d22925595047e24f21785e87596431c758
Alexoat76 <3915@holbertonschool.com>
2022-01-31 20:46:44 -0500

    Returns the Exact same object

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File: 1-my_list.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
