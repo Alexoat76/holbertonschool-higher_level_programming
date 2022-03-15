6a84d3662ccfad3ef27a1611c0a56dff479b74b7
Alexoat76 <3915@holbertonschool.com>
2022-03-15 16:03:01 -0500

    script | Updates the score of Bob to 10

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+/*File: 12-no_cheating.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Updates the score of Bob to 10 in the table second_table.
+UPDATE second_table SET score=10 WHERE name='Bob';
