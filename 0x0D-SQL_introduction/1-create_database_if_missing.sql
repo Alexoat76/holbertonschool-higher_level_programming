0789db5acfc4d01b0571e3ee8b954d62e2373840
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:28:48 -0500

    script | Creates the db hbtn_0c_0 | MySQL server

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+/*File: 1-create_database_if_missing.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Creates the database; shouldn't fail if non-existent
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
