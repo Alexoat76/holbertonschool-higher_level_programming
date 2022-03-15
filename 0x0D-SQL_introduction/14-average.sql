0bde28346a62ce6adfa6e671eb22a65a4204d569
Alexoat76 <3915@holbertonschool.com>
2022-03-15 16:09:18 -0500

    script | computes the score average of all records

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+/*File: 14-average.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Prints the average of records in a table in the database
+SELECT AVG(score) AS average FROM second_table;
