f78f1c84e2c51324cf39cebc8789021934bb1c43
Alexoat76 <3915@holbertonschool.com>
2022-03-15 16:07:28 -0500

    script | removes all records with a score <= 5

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+/*File: 13-change_class.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Removes all records with a score <= 5 in the table second_table of db.
+DELETE FROM second_table WHERE score <= 5;
