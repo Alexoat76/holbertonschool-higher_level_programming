1b757315e751d04b37bcd8ea029879504e6b4c9a
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:36:41 -0500

    script | Creates | table | first_table in current db | MySQL server

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+/*File: 4-first_table.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Creates a table called first_table with values id and name.
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
