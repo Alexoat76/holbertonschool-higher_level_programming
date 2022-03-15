4405a23244df6f3dc9783a218283f1052569716b
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:51:22 -0500

    script| Displays the number of records with id = 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+/*File: 8-count_89.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Counts records that pass a condition in a table in the database
+SELECT COUNT(*) FROM first_table WHERE id = 89;
