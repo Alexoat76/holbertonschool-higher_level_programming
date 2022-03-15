24d63d51f8f144f7ebc177e6d2e23a47a3ce87ff
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:38:40 -0500

    script|prints|full description|first_table|db hbtn_0c_0|MySQL server

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+/*File: 5-full_table.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Prints the full description of a table in the database
+SHOW CREATE TABLE first_table;
