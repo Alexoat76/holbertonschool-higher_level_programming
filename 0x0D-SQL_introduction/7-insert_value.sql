60af83e59ef91aee88be2ce2e03bf88cb7d2ebed
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:45:08 -0500

    script|inserts new row|first_table|db hbtn_0c_0|MySQL server

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+/*File: 7-insert_value.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Inserts a new row into the table first_table.
+INSERT INTO first_table (id, name) VALUES ('89', 'Best School');
