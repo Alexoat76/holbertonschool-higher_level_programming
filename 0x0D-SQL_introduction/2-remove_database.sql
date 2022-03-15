2903dbacb24b9fe17c3d1e7d0787f2eb06bb5644
Alexoat76 <3915@holbertonschool.com>
2022-03-15 15:32:43 -0500

    script | Deletes db hbtn_0c_0 | MySQL server

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,6 @@
+/*File: 2-remove_database.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Deletes the database; shouldn't fail if non-existent
+DROP DATABASE IF EXISTS hbtn_0c_0;
