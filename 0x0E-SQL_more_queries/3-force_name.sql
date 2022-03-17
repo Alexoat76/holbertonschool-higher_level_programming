09dcb335e99300c4769028739a78294ca8de19dc
Alexoat76 <3915@holbertonschool.com>
2022-03-17 11:48:00 -0500

    script | creates table force_name on | MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+/*File: 3-force_name.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Creates the table force_name on your MySQL server.
+-- to not be null
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
