2865a0e2e04dcb929d866a0c980e9003154a447c
Alexoat76 <3915@holbertonschool.com>
2022-03-17 12:24:48 -0500

    script | lists all genres | hbtn_0d_tvshows & displays the number of shows linked to each

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,14 @@
+/*File: 13-count_shows_by_genre.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- shows linked to each.
+-- Does not display genres without linked shows.
+-- Records are ordered by descending number of shows linked.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
