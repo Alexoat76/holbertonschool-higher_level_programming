fd43db536bad780c89f8c1fd3e6f99cc4febd096
Alexoat76 <3915@holbertonschool.com>
2022-03-17 11:52:08 -0500

    script | creates db hbtn_0d_usa & table states | MySQL server

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+/*File: 6-states.sql
+Author: Alex Orland Arévalo Tribaldos
+email: <3915@holbertonschool.com>*/
+
+-- Creates a database that doesn't overwrite pre-existing versions
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Switches the active database
+USE hbtn_0d_usa;
+-- Creates a table with a column that has automatically generated values
+-- and is also the primary key
+CREATE TABLE IF NOT EXISTS states(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL, UNIQUE(id));
