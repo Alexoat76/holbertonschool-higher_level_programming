6bb0aa4e61950828963815fd1b4a705de4cfbaf7
Alexoat76 <3915@holbertonschool.com>
2022-04-04 12:31:10 -0500

    Script| Prints “JavaScript is amazing”:heavy_check_mark:

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/** File: 0-javascript_is_amazing.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
