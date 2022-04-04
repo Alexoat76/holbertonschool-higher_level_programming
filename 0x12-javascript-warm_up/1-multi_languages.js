68a7292c311c4038f2fcd5eae3ec93df18181335
Alexoat76 <3915@holbertonschool.com>
2022-04-04 12:40:49 -0500

    Script| Prints 3 lines :heavy_check_mark:

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 /** File: 1-multi_languages.js
 *   Author: Alex Orland Arévalo Tribaldos
 *   email: <3915@holbertonschool.com> */
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
 
 /* Other solution
 ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
