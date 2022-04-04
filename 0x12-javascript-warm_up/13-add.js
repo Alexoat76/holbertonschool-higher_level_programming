a3f904db6bcfddef6be4c7d93e221bf55764428b
Alexoat76 <3915@holbertonschool.com>
2022-04-04 16:28:08 -0500

    Function| Returns the addition of 2 integers :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/** File: 13-add.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+module.exports = {
+/*
+* add - Computes the sum of 2 numbers.
+* @param {Number} a - The first number.
+* @param {Number} b - The second number.
+* @return The sum of the 2 numbers.
+*/
+  add: (a, b) => a + b
+};
