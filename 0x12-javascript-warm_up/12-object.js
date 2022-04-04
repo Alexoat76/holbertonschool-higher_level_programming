9fbc1c24964164657e9292728faf4256d4e9933f
Alexoat76 <3915@holbertonschool.com>
2022-04-04 16:26:17 -0500

    Update| Script to replace the value 12 with 89 :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/** File: 12-object.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
