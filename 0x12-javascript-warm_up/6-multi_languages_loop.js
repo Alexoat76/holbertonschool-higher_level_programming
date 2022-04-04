0ab0f33197f6928490b260b5e48e6fe9be9c900d
Alexoat76 <3915@holbertonschool.com>
2022-04-04 16:04:59 -0500

    Script| Prints 3 lines | with conditions :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/** File: 6-multi_languages_loop.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
