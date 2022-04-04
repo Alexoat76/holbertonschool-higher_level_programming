5aec959b62e02c47dc7cd9ad5eae955f5d245748
Alexoat76 <3915@holbertonschool.com>
2022-04-04 12:37:48 -0500

    Script| Prints 2 args passed to it, in the following format: “is” :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/** File: 4-concat.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+const arg0 = process.argv[2];
+const arg1 = process.argv[3];
+console.log(arg0 + ' is ' + arg1);
