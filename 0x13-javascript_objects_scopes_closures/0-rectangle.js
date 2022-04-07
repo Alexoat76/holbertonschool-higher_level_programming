1e5a844952520e2bca5daead3fb0e23ae8a77428
Alexoat76 <3915@holbertonschool.com>
2022-04-07 08:29:14 -0500

    Empty class Rectangle| Defines a rectangle :heavy_check_mark:

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/** File: 0-rectangle.js
+/** File:  0-rectangle.js
 *   Author: Alex Orland Arévalo Tribaldos
 *   email: <3915@holbertonschool.com> */
 module.exports = class Rectangle {};
