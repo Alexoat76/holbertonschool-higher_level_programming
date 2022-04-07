c6f936fb0d165d1d083963669231c4e98a2e6d65
Alexoat76 <3915@holbertonschool.com>
2022-04-07 08:25:15 -0500

    Class Rectangle| Defines a rectangle with conditions :heavy_check_mark:

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/** File: 1-rectangle.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
