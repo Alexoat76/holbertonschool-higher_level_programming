29807db904cf299db1e6a12b3ac3bbc51dac8566
Alexoat76 <3915@holbertonschool.com>
2022-04-11 19:12:56 -0500

    :new: Function| Increments and calls a function :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/** File: 102-add_me_maybe.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+exports.addMeMaybe = function (number, theFunction) {
+		number++;
+		theFunction(number);
+};
