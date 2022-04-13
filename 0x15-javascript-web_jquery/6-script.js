64fb03d0e4371dacd2172737ba13f7bbefb2ea85
Alexoat76 <3915@holbertonschool.com>
2022-04-12 19:20:21 -0500

    JS Script| Updates text of the <header> element to New Headergit add 6-script.js! when User clicks on:heavy_check_mark:

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+/** File:  6-script.js
+*   Author: Alex Orland Arévalo Tribaldos
+*   email: <3915@holbertonschool.com> */
+$(() => {
+  $('DIV#update_header').click(() => {
+    $('header').text('New Header!!!');
+  });
+});
