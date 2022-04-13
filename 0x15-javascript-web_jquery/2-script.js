ad43a4c3d8dc5f515c8d51a8f8990faacf70499c
Alexoat76 <3915@holbertonschool.com>
2022-04-12 19:14:10 -0500

    JS Script| Updates text color of the <header> element|red|When| user clicks on tag::heavy_check_mark:

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,8 +1,6 @@
-#!/usr/bin/node
 /** File:  2-script.js
 *   Author: Alex Orland Arévalo Tribaldos
 *   email: <3915@holbertonschool.com> */
-
 $(() => {
   $('DIV#red_header').click(() => {
     $('header').css('color', '#FF0000');
