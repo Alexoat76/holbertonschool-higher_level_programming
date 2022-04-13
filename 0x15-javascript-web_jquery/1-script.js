044e7b131947dae0c03be468611ab700981f183d
Alexoat76 <3915@holbertonschool.com>
2022-04-12 19:13:46 -0500

    JS Script| Updates text color of the <header> element| red (#FF0000)::heavy_check_mark:

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,8 +1,6 @@
-#!/usr/bin/node
 /** File:  1-script.js
 *   Author: Alex Orland Arévalo Tribaldos
 *   email: <3915@holbertonschool.com> */
-
 $(() => {
   $('header').css('color', '#FF0000');
 });
