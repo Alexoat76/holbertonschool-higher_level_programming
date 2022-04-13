09a32f3119699b2fafbe9cb6f7a7c4a585109097
Alexoat76 <3915@holbertonschool.com>
2022-04-12 19:22:06 -0500

    JS Script| Updates text of the <header> element to New Header| when User clicks on:heavy_check_mark:

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,6 @@
 /** File:  6-script.js
 *   Author: Alex Orland Arévalo Tribaldos
-*   email: <3915@holbertonschool.com> */
+*   email: <3915@holbertonschool.com>  */
 $(() => {
   $('DIV#update_header').click(() => {
     $('header').text('New Header!!!');
