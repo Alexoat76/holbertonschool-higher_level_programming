c43b0cb8ab4908a0de6f12a05dc13223626fae77
Alexoat76 <3915@holbertonschool.com>
2022-04-12 19:14:35 -0500

    JS Script| Adds the class red| the <header> element|When|User clicks on the tag:heavy_check_mark:

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,8 +1,6 @@
-#!/usr/bin/node
 /** File:  3-script.js
 *   Author: Alex Orland Arévalo Tribaldos
 *   email: <3915@holbertonschool.com> */
-
 $(() => {
   $('DIV#red_header').click(() => {
     if (!$('header').hasClass('red')) {
