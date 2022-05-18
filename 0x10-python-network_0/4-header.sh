031f72e28cc3f218be170809350bf4b9b144cb2a
Alexoat76 <3915@holbertonschool.com>
2022-05-18 13:15:30 -0500

    Script| takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in URL, add header variable, displays "Hello School!"; Usage: ./4-header.sh 0.0.0.0:5000/route_5 ; echo ""
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
