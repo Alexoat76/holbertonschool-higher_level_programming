4aa904cb5d54d4bba13b46da823d40334cdf4dd1
Alexoat76 <3915@holbertonschool.com>
2022-05-19 14:27:47 -0500

    Python Script| fetches https://intranet.hbtn.io/status :heavy_check_mark:

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# File: 4-hbtn_status.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+"""
+fetch https://intranet.hbtn.io/status; display response
+"""
+import requests
+
+
+if __name__ == '__main__':
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
